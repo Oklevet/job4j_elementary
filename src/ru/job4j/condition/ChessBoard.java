268b3869a5ced85aa8fb4074e7d017669e5778d1
”Oklevet” <nikstar1235@gmail.com>
2020-12-15 12:58:21 +0500

    4.6.2. Switch - matcher [#430080]

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -6,8 +6,7 @@ public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         if (abs(x1 - x2) == abs(y1 - y2)) {
             return abs(x1 - x2);
-        } else {
-            return 0;
         }
+        return 0;
     }
 }
