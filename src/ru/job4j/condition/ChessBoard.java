a4594da6f5b2d314b44fcfcc30a52551231d8fb9
”Oklevet” <nikstar1235@gmail.com>
2020-12-15 10:21:54 +0500

    4.3.6. Math.abs [#430082]

---
+++ src/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+import static java.lang.StrictMath.abs;
+
+public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2) {
+        if (abs(x1 - x2) == abs(y1 - y2)) {
+            return abs(x1 - x2);
+        } else {
+            return 0;
+        }
+    }
+}
