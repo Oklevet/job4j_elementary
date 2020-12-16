d7b4221b0482a40e6b261ab43e46770a785d300e
”Oklevet” <nikstar1235@gmail.com>
2020-12-17 04:56:21 +0500

    6.0.2. Заполнение массива. [#430104]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -6,8 +6,17 @@ public class ArrayDefinition {
         String[] surnames = new String[100500];
         float[] prices = new float[40];
 
+        String[] names = new String[4];
+        names[0] = "qwe qweeqw";
+        names[1] = "asd asdgfd";
+        names[2] = "zxc zxczcv";
+        names[3] = "qweasd zxcasd";
+
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        for (String s : names) {
+            System.out.println(s);
+        }
     }
 }
