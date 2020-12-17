42cde923fb86ef4d190e08d13b2aa6fb68ebdec0
”Oklevet” <nikstar1235@gmail.com>
2020-12-17 05:36:37 +0500

    6.0.5. Массивы в тестах. [#430106]

---
+++ test/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] input = {5, 4, 3, 2};
+        int value = 5;
+        int result = FindLoop.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHasNot11ThenFalse() {
+        int[] input = {4, 6, 9, 6, 3};
+        int value = 11;
+        int result = FindLoop.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+}
