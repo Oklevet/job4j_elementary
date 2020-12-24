6529f55b862fa8d8e04bc9fdc0384b0c52839d9e
Oklevet <nikstar1235@gmail.com>
2020-12-24 15:30:23 +0300

    1.8. Состояние объекта [#430289]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,19 +1,27 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        return rsl;
+
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.x - that.y, 2));
+
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        double result1 = Point.distance(1, 0, 2, 4);
-        double result2 = Point.distance(-14, -1, 13, 12);
-        double result3 = Point.distance(3, -2, 3, -2);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        System.out.println("result (1, 0) to (2, 4) " + result1);
-        System.out.println("result (-14, -1) to (13, 12) " + result2);
-        System.out.println("result (3, -2) to (3, -2) " + result3);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
