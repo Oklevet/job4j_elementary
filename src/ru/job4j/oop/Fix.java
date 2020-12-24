0a26b15be75567d2afe631b19add99cb554e9449
Oklevet <nikstar1235@gmail.com>
2020-12-24 12:42:17 +0300

    1.7. Конструктор [#430290]

---
+++ src/ru/job4j/oop/Fix.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desc;
+
+    public Fix(String text) {
+        desc = text;
+    }
+
+    public static void main(String[] args) {
+        String issue = "Will fix ASAP";
+        Fix fix = new Fix(issue);
+    }
+}
