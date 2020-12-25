80024c3ec67dbf94b335fb6796709ffd44be7691
Oklevet <nikstar1235@gmail.com>
2020-12-25 09:44:30 +0300

    2. Перегрузка конструктора [#430295]

---
+++ src/ru/job4j/tracker/Item.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+public class Item {
+    private int id;
+    private String name;
+
+    public Item() {
+    }
+
+    public Item(int id) {
+        this.id = id;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        Item item1 = new Item(12);
+        Item item2 = new Item(33, "qwerty");
+    }
+}
